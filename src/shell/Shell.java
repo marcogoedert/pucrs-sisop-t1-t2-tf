4df3b5f8f053c3ec16f191d54cbfbd4b5968e0f2
Marco Goedert <marco.goedert@edu.pucrs.br>
2020-03-23 00:30:52 -0300

    Update shell

--- src/shell/Shell.java
+++ src/shell/Shell.java
@@ -1,5 +1,20 @@
 package shell;
 
 public class Shell {
-
+	public static void main(String args[]) {
+		System.out.println("╔══════════════════════════════════════╗");
+		System.out.println("║              Welcome                 ║");
+		System.out.println("║                                      ║");
+		System.out.println("║Programs:                             ║");
+		System.out.println("║                                      ║");
+		System.out.println("║ - fibonacci_10                       ║");
+		System.out.println("║ - fibonnaci_n                        ║");
+		System.out.println("║ - fatorial                           ║");
+		System.out.println("║ - bubblesort                         ║");
+		System.out.println("║                                      ║");
+		System.out.println("╚══════════════════════════════════════╝");
+		System.out.print("Tip: write the name of the program to run it.");
+		
+		
+	}
 }
